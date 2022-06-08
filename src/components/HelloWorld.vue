4d99cb4beb582d949e790b822874a2aa7097f54c
andycodev <andycodev@gmail.com>
2022-06-08 16:51:38 -0500

    adding project to github

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,15 @@
 <template>
-  <h1>{{ msg }} {{name}}</h1>
+  <div>
+    <h1>{{ msg }} {{ name }}</h1>
 
-  <div class="row">
-    <button type="button" @click="download_file(urlFile, nameFile)">Dercargar Archivo</button>
+    <div class="row">
+      <button type="button" @click="download_file(urlFile, nameFile)">
+        Dercargar Archivo
+      </button>
+    </div>
   </div>
 
- <!--  <p>
+  <!--  <p>
     Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
     +
@@ -28,52 +32,53 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 defineProps({
   msg: String,
-  name: String
-})
+  name: String,
+});
 
 /* const count = ref(0) */
 
-const urlFile = 'https://andycodev.github.io/deploying-vite-project-test/assets/archivo.pdf';
-const nameFile = 'archivoCV';
+const urlFile =
+  "https://andycodev.github.io/deploying-vite-project-test/assets/archivo.pdf";
+const nameFile = "archivoCV";
 
 /* Helper function */
 function download_file(fileURL, fileName) {
-    // for non-IE
-    if (!window.ActiveXObject) {
-        var save = document.createElement('a');
-        save.href = fileURL;
-        save.target = '_blank';
-        var filename = fileURL.substring(fileURL.lastIndexOf('/')+1);
-        save.download = fileName || filename;
-	       if ( navigator.userAgent.toLowerCase().match(/(ipad|iphone|safari)/) && navigator.userAgent.search("Chrome") < 0) {
-				document.location = save.href; 
-// window event not working here
-			}else{
-		        var evt = new MouseEvent('click', {
-		            'view': window,
-		            'bubbles': true,
-		            'cancelable': false
-		        });
-		        save.dispatchEvent(evt);
-		        (window.URL || window.webkitURL).revokeObjectURL(save.href);
-			}	
+  // for non-IE
+  if (!window.ActiveXObject) {
+    var save = document.createElement("a");
+    save.href = fileURL;
+    save.target = "_blank";
+    var filename = fileURL.substring(fileURL.lastIndexOf("/") + 1);
+    save.download = fileName || filename;
+    if (
+      navigator.userAgent.toLowerCase().match(/(ipad|iphone|safari)/) &&
+      navigator.userAgent.search("Chrome") < 0
+    ) {
+      document.location = save.href;
+      // window event not working here
+    } else {
+      var evt = new MouseEvent("click", {
+        view: window,
+        bubbles: true,
+        cancelable: false,
+      });
+      save.dispatchEvent(evt);
+      (window.URL || window.webkitURL).revokeObjectURL(save.href);
     }
+  }
 
-    // for IE < 11
-    else if ( !! window.ActiveXObject && document.execCommand)     {
-        var _window = window.open(fileURL, '_blank');
-        _window.document.close();
-        _window.document.execCommand('SaveAs', true, fileName || fileURL)
-        _window.close();
-    }
+  // for IE < 11
+  else if (!!window.ActiveXObject && document.execCommand) {
+    var _window = window.open(fileURL, "_blank");
+    _window.document.close();
+    _window.document.execCommand("SaveAs", true, fileName || fileURL);
+    _window.close();
+  }
 }
-
-
-
 </script>
 
 <style scoped>
